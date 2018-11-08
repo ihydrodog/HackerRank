aebc2468539b77e599368cea643b4fea5095bcf3
hydro <ihydrodog@gmail.com>
2018-11-09 00:30:55 +0900

    save progress

--- HourRank 30/JerrysExpression.py
+++ HourRank 30/JerrysExpression.py
@@ -37,31 +37,50 @@ def __calc( op, l, r):
     elif op == '-':
         return l-r
 
-def solve(expression):
 
-    # res = []
-    # _calculate( queue, res, 0 )
-
+def calculate( expression, res = None, debt = None, debtForPositiveOperand = None, debtForNegativeOperand = None ):
     operators = ['-','+']
-    expression = list(expression)
+
 
     stack = [ expression.pop(0) ]
+
+    curSign = 1 # 1 means positive sign
+
+    positiveCount, negativeCount = 0, 0
 
     while len(stack) > 1 or expression:
         c = stack[-1]
 
         if c in operators:
+            if c == '-':
+                curSign = -curSign
+
             # need left operand
             stack.append(expression.pop(0))
         else:
             if stack[-2] not in operators:
                 right, left = stack.pop(), stack.pop()
+                operator = stack.pop()
                 if left == '?':
+                    if curSign > 0:
+                        positiveCount+=1
+                    else:
+                        negativeCount+=1
                     left = 1
                 if right == '?':
+                    if curSign > 0:
+                        if operator == '+':
+                            positiveCount+=1
+                        else:
+                            negativeCount+=1
+                    else:
+                        if operator == '-':
+                            positiveCount+=1
+                        else:
+                            negativeCount+=1
                     right = 1
 
-                operator = stack.pop()
+                curSign = operator == '+'
 
                 result = left + right if operator=='+' else left - right
                 stack.append( result )
@@ -69,8 +88,26 @@ def solve(expression):
                 # need right operand
                 stack.append(expression.pop(0))
 
-    return stack[0]
+    return stack[0], (positiveCount, negativeCount)
 
+def solve(expression):
+    traversed = list(expression)
+    sumIfQuestionMarkIsOne, (positiveCount, negativeCount) = calculate( traversed, None )
+
+    debtForPositiveOperand, debtForNegativeOperand = 0, 0
+    if sum > 0:
+        debt = sum
+        debtForNegativeOperand = math.ceil( debt/negativeCount )
+    elif sum < 0:
+        debt = -sum
+        debtForPositiveOperand = math.ceil( debt/positiveCount )
+
+    traversed = list(expression)
+
+    result = []
+    _, _ = calculate( traversed, result, debt, debtForPositiveOperand, debtForNegativeOperand )
+
+    return result
 
 print( solve( '-?-??'))
 

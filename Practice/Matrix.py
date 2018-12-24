66bc9a0c6ed8f183cc155dffa33c65627edb499d
hydro <ihydrodog@gmail.com>
2018-12-24 09:50:18 +0900

    save progress

--- Practice/Matrix.py
+++ Practice/Matrix.py
@@ -7,66 +7,65 @@ import re
 import sys
 
 
-def findPath( edges, a, b):
-
-    queue = [(a, [a])]
+def findPathAndCut( edges, costTable, machines, start ):
+    queue = list( )
+    queue.append( ( start, list( (start,) ) ) )
     visited = set()
 
     while queue:
-        cur, path = queue.pop(0)
+        curNode, path = queue.pop( 0 )
+        visited.add( curNode )
 
-        visited.add( cur )
+        if curNode in machines and curNode != start :
 
-        for adjacent in (edges[cur]-visited):
-            newPath = list(path)
-            newPath.append( adjacent )
-            if adjacent == b:
-                return newPath
-            else:
-                queue.append( (adjacent, newPath) )
+            # find the chepeast and cut
+            cheapestNode = None
+            cheapestCost = None
+            for index in range(len(path) - 1):
+                n0 = path[index]
+                n1 = path[index + 1]
+                edgeCost = costTable[(n0, n1) if n0 < n1 else (n1, n0)]
+                if cheapestCost is None or cheapestCost > edgeCost:
+                    cheapestCost = edgeCost
+                    cheapestNode = (n0, n1)
 
-    return None
 
+            n0, n1 = cheapestNode
+            edges[ n0 ].remove( n1 )
+            edges[ n1 ].remove( n0 )
 
+            return cheapestCost, ( start, curNode )
+        else:
+            for n in edges[curNode]-visited:
+                newPath = list( path )
+                newPath.append( n )
+                queue.append( ( n, newPath ) )
 
+    return None, (None, None)
 
 # Complete the minTime function below.
 def minTime(roads, machines):
     edges = {}
 
-    cost = {}
+    costTable = {}
     for r in roads:
         a, b = r[0], r[1]
         edges.setdefault( a, set() ).add( b )
         edges.setdefault( b, set() ).add( a )
 
-        cost[ ( a, b ) if a<b else (b, a) ] = r[2]
+        costTable[ ( a, b ) if a<b else (b, a) ] = r[2]
 
     costSum = 0
     # find paths
-    path = {}
-    machineCount = len( machines )
-    for a in range( machineCount ):
-        for b in range( a ):
-            p = findPath( edges, machines[a], machines[b] )
-            if p:
-                cheapestNode = None
-                cheapestCost = None
-                for index in range( len( p )-1):
-                    n0 = p[index]
-                    n1 = p[index+1]
-                    edgeCost = cost[ ( n0, n1 ) if n0<n1 else (n1, n0) ]
-                    if cheapestCost is None or cheapestCost > edgeCost:
-                        cheapestCost = edgeCost
-                        cheapestNode = ( n0, n1)
 
-                # cut the cheapest
-                n0, n1 = cheapestNode
-                edges[ n0 ].remove( n1 )
-                edges[ n1 ].remove( n0 )
-                costSum += cheapestCost
-
-
+    queue = list( (machines[0], ))
+    while queue:
+        start = queue.pop()
+        cost, (n0, n1) = findPathAndCut( edges, costTable, machines, start )
+        if cost is not None:
+            queue.append( n0 )
+            queue.append( n1 )
+            costSum += cost
 
 
     return costSum
